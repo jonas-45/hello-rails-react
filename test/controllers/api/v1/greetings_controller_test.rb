1fd4d008da38ce4c23f8fc696b184c33fcaf0d7c
jonas-45 <jonaswullah@gmail.com>
2023-08-28 06:48:28 +0000

    Add linters configurations

--- test/controllers/api/v1/greetings_controller_test.rb
+++ test/controllers/api/v1/greetings_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::GreetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_v1_greetings_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get api_v1_greetings_show_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get api_v1_greetings_create_url
     assert_response :success
   end
--- test/controllers/api/v1/random_greetings_controller_test.rb
+++ test/controllers/api/v1/random_greetings_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::RandomGreetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_v1_random_greetings_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get api_v1_random_greetings_show_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get api_v1_random_greetings_create_url
     assert_response :success
   end
